7afd5ca12ef161aa9181b53c5e7971c87014d4cc
Gojo-script <gojoscript@gmail.com>
2024-11-15 15:44:13 +0000

    Galaxy Hub🪐 V3

--- Bananahubbeta.lua
+++ Bananahubbeta.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Banana Hub-Blox Fruit[Beta]",
-    SubTitle = "By Obii",
+    Title = "Galaxy Hub V3🪐",
+    SubTitle = "By Gojo.Script",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
